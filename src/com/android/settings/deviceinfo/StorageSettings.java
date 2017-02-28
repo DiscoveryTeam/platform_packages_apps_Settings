fe6fa0769c2e5e6811ceb08788969d6fcddf7271
Bryan Owens <djbryan3540@gmail.com>
2017-02-27 22:24:09 -0600

    [1/2] Settings: Expose "ALL" hardcoded and @android colors
    Change-Id: Iac46ea78387881e2237c3b9fb302c84987fe94f3
    Signed-off-by: Bryan Owens <djbryan3540@gmail.com>

--- src/com/android/settings/deviceinfo/StorageSettings.java
+++ src/com/android/settings/deviceinfo/StorageSettings.java
@@ -26,6 +26,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.UserHandle;
